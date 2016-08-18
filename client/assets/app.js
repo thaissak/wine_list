fbe3a12381b8737612417cfe50fe423fdc642a06
thaissak <thaissa.kobayashi@gmail.com>
2016-08-18 15:22:39 -0700

    client: added partials for wines and wineries, partial CSS for all main, login, registration, profile, wine and winery

--- client/assets/app.js
+++ client/assets/app.js
@@ -21,44 +21,89 @@ app.config(function($routeProvider){
 
 // LOGGED USER
 	.when('/Home', {
-		templateUrl: 'partial/home.html',
+		templateUrl: 'partial/user_home.html',
 		controller: 'userController'
 	})
 
 // PROFILE
 	.when('/Profile', {
-		templateUrl: 'partial/profile.html',
+		templateUrl: 'partial/user_profile.html',
 		controller: 'userController'
 	})
 
 	.when('/Profile/Edit', {
-		templateUrl: 'partial/editUser.html',
+		templateUrl: 'partial/user_edit.html',
 		controller: 'userController'
 	})
 
 	.when('/Profile/Edit/Password', {
-		templateUrl: 'partial/changePwd.html',
+		templateUrl: 'partial/user_changePwd.html',
 		controller: 'userController'
 	})
 
 // WINES
 	.when('/Wines', {
-		templateUrl: 'partial/wines.html',
+		templateUrl: 'partial/wine.html',
+		controller: 'wineController'
+	})
+
+	.when('/Wines/New', {
+		templateUrl: 'partial/wine_add.html',
+		controller: 'wineController'
+	})
+
+	.when('/Wines/Edit', {
+		templateUrl: 'partial/wine_edit.html',
+		controller: 'wineController'
+	})
+
+	.when('/Wines/Show', {
+		templateUrl: 'partial/wine_show.html',
 		controller: 'wineController'
 	})
 
 
 //WINERIES
 	.when('/Wineries', {
-		templateUrl: 'partial/wineries.html',
+		templateUrl: 'partial/winery.html',
 		controller: 'wineriesController'
 	})
 
-// LISTS
-	.when('/Lists', {
-		templateUrl: 'partial/lists.html',
-		controller: 'listsController'
+	.when('/Wineries/New', {
+		templateUrl: 'partial/winery_add.html',
+		controller: 'wineryController'
 	})
+
+	.when('/Wineries/Edit', {
+		templateUrl: 'partial/winery_edit.html',
+		controller: 'wineryController'
+	})
+
+	.when('/Wineries/Show', {
+		templateUrl: 'partial/winery_show.html',
+		controller: 'wineryController'
+	})
+
+// LISTS
+	// .when('/Lists', {
+	// 	templateUrl: 'partial/lists.html',
+	// 	controller: 'listsController'
+	// })
+
+	// .when('/Lists/New', {
+	// 	templateUrl: 'partial/add_list.html',
+	// 	controller: 'listController'
+	// })
+
+	// .when('/Lists/Edit', {
+	// 	templateUrl: 'partial/edit_list.html',
+	// 	controller: 'listController'
+	// })
+
+	// .when('/Lists/Show', {
+	// 	templateUrl: 'partial/show_list.html',
+	// 	controller: 'listController'
+	// })
 
 // OTHER
 	.otherwise({
